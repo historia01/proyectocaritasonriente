18dac51ace8c058bf0fa27eb73212ba9f0bf63ec
historia <tuyulewds@gmail.com>
2023-04-21 15:09:06 -0500

    Added symbols plus description

--- scripts/guideOthers.js
+++ scripts/guideOthers.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    //Left Sidebar recommendations
     $("#otherMenu").click(function(){ 
         if($(".sidebar").css("left")=="-300px"){
             $(".sidebar").css("left", "0");
@@ -6,5 +7,22 @@ $(document).ready(function() {
         else {
             $(".sidebar").css("left", "-300px");
         }
+    });
+    //Right sidebar graphics
+    //Credits menu
+    //Modify text
+    $("#compareButton").click(function() {
+        console.log("loaded Function");
+        if($("#numberB").val() == $("#numberD").val()){
+            $("#commentHeader").text('Mismo Denominador');
+            $("#commentPa1").html('En estos casos se mantiene el<br>mismo numero del denominador y<br>se suma o resta sus denominadores');
+            console.log("change OK");
+        }
+        else {
+            $("#commentHeader").text('Diferente Denominador');
+            $("#commentPa1").html('En este caso se multiplica los<br>denominadores y se multiplican en forma de X<br>como muestra el gráfico');
+        }
+        
+        
     });
 });
--- scripts/main.js
+++ scripts/main.js
@@ -16,10 +16,32 @@ B                D                  G             RB                RD
 a = 0, b = 1, c = 2, d = 3, e = 4, f = 5, g = 6, c(abs) = 7, d(abs) = 8, e(abs) = 9, f(abs) = 10         
 */
 function solveFraction(param11, param12, param21, param22, paramOp){
-    let a = param11 * param22;
-    let b = param12 * param21;
-    let c = (param12 == param22) ? param22 : param12 * param22;
-    let d = (paramOp == 1) ? a + b : (a - b);
+    let a = 0;
+    let b = 0;
+    let c = 0;
+    let d = 0;
+    if(param12 == param22){
+        a = param11;
+        b = param21;
+        c = param22;
+    }
+    else {
+        a = param11 * param22;
+        b = param12 * param21;
+        c = param12 * param22;
+    }
+    console.log(typeof a);
+    //let d = (paramOp == 1) ? Number(a) + Number(b) : (Number(a) - Number(b));
+    if(paramOp == 1){
+        d = Number(a) + Number(b);
+        $(".opSign").text("+");
+        console.log("operation Add" + d)
+    }
+    else {
+        d = Number(a) - Number(b);
+        $(".opSign").text("-");
+        console.log("operation Substract" + d)
+    }
     let e = ((math.abs(d*c))/(math.gcd(((d<0)?d*-1:d),c)))/c;
     let f = ((math.abs(d*c))/(math.gcd(((d<0)?d*-1:d),c)))/d;
     let g = (e < 0 || f < 0 ) ? 0 : 1;
@@ -43,7 +65,7 @@ $(document).ready(function() {
         $('#numberRB').val(fractionResults[2]);
         $('#numberRC').val(fractionResults[9]);
         $('#numberRD').val(fractionResults[10]);
-        $('.resultOperator').text((fractionResults[6]==1)?'+ ——————':'- ——————');
+        $('.resultOperator').text((fractionResults[6]==1)?'+ ————':'- ————');
     });
     $("#clearButton").click(function() {
         $('#numberA').val('');
@@ -57,5 +79,16 @@ $(document).ready(function() {
         $('#numberRB').val('');
         $('#numberRC').val('');
         $('#numberRD').val('');
+        $(".opSign").text('±');
+        $("#numberA").css("border-color", "#4e2c0b");
+        $("#numberA").css("background-color", "#7d4714");
+        $("#numberB").css("border-color", "#4e2c0b");
+        $("#numberB").css("background-color", "#7d4714");
+        $("#numberC").css("border-color", "#4e2c0b");
+        $("#numberC").css("background-color", "#7d4714");
+        $("#numberD").css("border-color", "#4e2c0b");
+        $("#numberD").css("background-color", "#7d4714");
+        $("#commentHeader").html('Bienvenido al Proyecto<br>Carita Sonriente!');
+        $("#commentPa1").html('¿Perdid@? <a id="howToUse">¡Haz clíc aquí!</a>');
     });
 });

b17e48a507bf24fa3a84a3c747d7d67c5c9fd886
historia <tuyulewds@gmail.com>
2023-04-24 12:47:34 -0500

    34

--- scripts/others.js
+++ scripts/others.js
@@ -1,28 +1,32 @@
 $(document).ready(function(){
+  var elem = document.documentElement;
+  function requestFS(){
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else {
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.mozRequestFullScreen();
+      } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+        elem.webkitRequestFullscreen();
+      } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        elem.msRequestFullscreen();
+      }
+    }
+    // check if screen orientation is supported
+    if(screen.orientation.lock) {
+    // lock screen to landscape orientation
+      screen.orientation.lock("landscape").then(function() {
+        console.log("Orientation locked to landscape");
+      }).catch(function(error) {
+        console.error("Failed to lock orientation", error);
+      });
+    }
+  }
+  requestFS();
     $("#fullscreenButton").click(function(){
-        var elem = document.documentElement;
-        if (document.fullscreenElement) {
-          document.exitFullscreen();
-        } else {
-          if (elem.requestFullscreen) {
-            elem.requestFullscreen();
-          } else if (elem.mozRequestFullScreen) { /* Firefox */
-            elem.mozRequestFullScreen();
-          } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
-            elem.webkitRequestFullscreen();
-          } else if (elem.msRequestFullscreen) { /* IE/Edge */
-            elem.msRequestFullscreen();
-          }
-        }
-        // check if screen orientation is supported
-        if(screen.orientation.lock) {
-          // lock screen to landscape orientation
-          screen.orientation.lock("landscape").then(function() {
-            console.log("Orientation locked to landscape");
-          }).catch(function(error) {
-            console.error("Failed to lock orientation", error);
-          });
-        }
+        requestFS();
     });
     $("button").click(function(){
       $("#click")[0].currentTime = 0;
